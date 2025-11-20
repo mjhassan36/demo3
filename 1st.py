4d170e5875090d1d82d86b3bedc9a958b5508c73
mjhassan36 <mjhassan36@gmail.com>
2025-11-21 01:42:08 +0600

    third commit

--- 1st.py
+++ 1st.py
@@ -5,3 +5,9 @@ print("i am a student of cse")
 print("i love programming")
 print("i love python programming")
 print("i love coding")
+
+
+print("i love problem solving")     
+print("i love learning new things")
+print("i love open source")
+print("i love github")
